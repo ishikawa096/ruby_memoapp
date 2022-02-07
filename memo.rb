4ff433be7be041097a110175d55011241a69b2fc
ishikawa096 <ishikawa096@gmail.com>
2022-02-07 22:14:03 +0900

    メモ編集機能を作成

--- memo.rb
+++ memo.rb
@@ -10,6 +10,7 @@ memo_type = gets.to_i
 
 # 2なら"既存のメモを表示します"編集したいメモの番号を入力してください"
 # 選ばれたメモを開く "編集が完了したらCtrl + Dを押します"
+
 if memo_type == 1
     puts "拡張子を除いたファイル名を入力してください" 
     memo_name = gets.chomp
@@ -44,30 +45,66 @@ if memo_type == 1
     puts "上記メモを作成しました。アプリを終了します。"
 elsif memo_type == 2
     puts "既存のメモを表示します"
-    puts "編集したいメモの番号を入力してください"
-    memo_type = gets.to_i
+    puts "------------------"
+    #puts "編集したいメモの番号を入力してください"
+    #memo_type = gets.to_i
 #番号つき一覧表示、選ばれた番号のファイル名を変数に入れる
-    open_name = #取得したファイル名
-    open_content = CSV.foreach("#{open_name}.csv")
-    puts "【ファイル名】#{open_name}" 
-    puts "【メモ内容】#{open_content}"
-    puts "このメモを編集しますか？"
-    puts "1(はい) 2(いいえ)"
-    memo_type = gets.to_i
-    if memo_type == 1
-        puts "編集後の内容を入力してください"
-        puts "完了したらCtrl + Dを押します"
-        new_content = $stdin.readlines
-        CSV.open("#{open_name}.csv" , "r+") do |csv|
-            csv << ["#{new_content}"]
-        end
-        puts "編集が完了しました。アプリを終了します。"
-        
+    Find.find('.') do |item|
+        puts item
     end
-    if memo_type == 2
-        ##既存のメモ一覧へ戻る？
+    #open_name = #取得したファイル名
+    puts "------------------"
+    while memo_type < 3 do
+        puts "開きたいファイル名を拡張子を除いて入力してください。"
+        begin
+            open_name = gets.chomp ##ファイル名を手動入力
+            open_content = CSV.foreach("#{open_name}.csv")
+            puts "------------------"
+            puts "【ファイル名】"
+            puts open_name
+            puts "【メモ内容】"
+            open_content.each do |content|
+                puts content
+            end
+        rescue
+            puts $!
+            puts "ファイルを開けませんでした。"
+            puts "もう一度お願いします。"
+            next
+        end
+        puts "------------------"
+        puts "このメモを編集しますか？"
+        puts "1(はい) 2(いいえ) 3他(キャンセルして終了)"
+        memo_type = gets.to_i
+        if memo_type == 1
+            puts "編集後の内容を入力してください"
+            puts "完了したらCtrl + Dを押します"
+            new_content = readlines
+            CSV.open("#{open_name}.csv","w") do |csv|
+                new_content.each do |content|
+                    write_content = content.chomp
+                    csv << [ write_content ]
+                end
+            end
+            puts ""
+            puts "------------------"
+            puts "【ファイル名】"
+            puts open_name
+            puts "【メモ内容】"
+            new_content.each do |content|
+                puts content
+            end
+            puts "------------------"
+            puts "編集が完了しました。アプリを終了します。"
+            break
+        elsif memo_type == 2
+            next
+        else
+            puts "アプリを終了します。"
+            next
+        end
     end
 
 else 
-    puts "1 か 2 を入力してください"
+   puts "1か2を入力してください"
 end
